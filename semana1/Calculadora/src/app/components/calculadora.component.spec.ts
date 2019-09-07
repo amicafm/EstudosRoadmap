4b89f7536d972b7dea868d7fce4eaa660d39c9f1
masamiles <36736874+masamiles@users.noreply.github.com>
2019-09-07 13:08:42 -0300

    testes

--- semana1/Calculadora/src/app/components/calculadora.component.spec.ts
+++ semana1/Calculadora/src/app/components/calculadora.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { CalculadoraComponent } from './calculadora.component';
+import { CalculadoraService } from '../services';
 
 describe('CalculadoraComponent', () => {
   let component: CalculadoraComponent;
@@ -8,7 +10,10 @@ describe('CalculadoraComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CalculadoraComponent ]
+      declarations: [ CalculadoraComponent ],
+      providers: [
+        CalculadoraService
+      ]
     })
     .compileComponents();
   }));
@@ -22,4 +27,27 @@ describe('CalculadoraComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('deve garantir que 3 + 2 = 5', () => {
+    let btn3 = fixture.debugElement.query(By.css('#btn3'));
+    let btnSoma = fixture.debugElement.query(By.css('#btnSoma'));
+    let btn2 = fixture.debugElement.query(By.css('#btn2'));
+    let btnCalcular = fixture.debugElement.query(By.css('#btnCalcular'));
+    let display = fixture.debugElement.query(By.css('#display'));
+
+    btn3.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    btnSoma.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    btn2.triggerEventHandler('click', null);
+    fixture.detectChanges();
+    
+    btnCalcular.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    expect(display.nativeElement.value).toEqual('5');
+  }); 
+
 });
--- semana1/Calculadora/src/app/services/calculadora.service.ts
+++ semana1/Calculadora/src/app/services/calculadora.service.ts
@@ -1,38 +1,57 @@
+/**
+ * Serviço responsável por executar as operações da 
+ * calculadora.
+ * 
+ * @author Márcio C. de Souza<contato@kazale.com>
+ * @since 1.0.0
+ */
+
 import { Injectable } from '@angular/core';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class CalculadoraService {
 
-  /* STATIC FACILITA O ACESSO, READONLY DIZ QUE ESSES VALORES NAO SERAO MODIFICADOS
-    E O NOME EM CAPS NOS INDICA QUE É UMA CONSTANTE*/
-  static readonly SOMA: string ='+';
-  static readonly SUBTRACAO: string ='-';
-  static readonly DIVISAO: string ='/';
-  static readonly MULTIPLICACAO: string ='*';
+  /* Define as constantes utilizadas 
+     para identificar as operações de cálculo */
+  static readonly SOMA: string = '+';
+  static readonly SUBTRACAO: string = '-';
+  static readonly DIVISAO: string = '/';
+  static readonly MULTIPLICACAO: string = '*';
 
   constructor() { }
 
-  calcular(num1: number, num2: number, operacao: string): number{
-    let resultado: number;
+  /**
+   * Método que calcula uma operação matemática dado 
+   * dois números.
+   * Suporta as operações soma, subtração, divisão, 
+   * e multiplicação.
+   *
+   * @param num1 number
+   * @param num2 number
+   * @param operacao string Operação a ser executada
+   * @return number Resultado da operação
+   */
+  calcular(num1: number, num2: number, operacao: string): number {
+  	let resultado: number; // armazena o resultado da operação
 
-    switch(operacao){
-      case CalculadoraService.SOMA:
-        resultado = num1 + num2;
-      break;
-      case CalculadoraService.SUBTRACAO:
-        resultado = num1 - num2;
-      break;
-      case CalculadoraService.DIVISAO:
-        resultado = num1 / num2;
-      break;
-      case CalculadoraService.MULTIPLICACAO:
-        resultado = num1 * num2;
-      break;
-      default:
-        resultado = 0; //em caso de operação invalida ou desconhecida
-    }
-      return resultado;
+  	switch(operacao) {
+  	  case CalculadoraService.SOMA:
+  	    resultado = num1 + num2;
+  		break;
+  	  case CalculadoraService.SUBTRACAO:
+  	    resultado = num1 - num2;
+  		break;
+  	  case CalculadoraService.DIVISAO:
+  	    resultado = num1 / num2;
+  		break;
+  	  case CalculadoraService.MULTIPLICACAO:
+  	    resultado = num1 * num2;
+  		break;
+  	  default:
+  	    resultado = 0;
+  	}
+
+  	return resultado;
   }
+
 }
